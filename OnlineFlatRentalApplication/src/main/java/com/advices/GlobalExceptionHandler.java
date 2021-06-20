baa617f2aec9f1c584a7d6759d469eba3c3ede3f
Lenovo <Lenovo@192.168.0.6>
2021-06-20 13:49:54 +0530

    Saileela_project users unit cases passed exceptiona

---
+++ OnlineFlatRentalApplication/src/main/java/com/advices/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.advices;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(UserNotFoundException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+	
+	  @ExceptionHandler(Exception.class) 
+	  public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) { ErrorDetails
+	  errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),request.getDescription(false)); 
+	  return new ResponseEntity<>(errorDetails,HttpStatus.INTERNAL_SERVER_ERROR); }
+	 
+}
---
+++ OnlineFlatRentalApplication/src/main/java/com/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.exception;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
+
